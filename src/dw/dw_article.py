36446b0f0a01f710fc9a6577e8b5220b9531de63
Victor Liu <victorlp2000@gmail.com>
2020-07-19 23:34:13 -0700

    fine-tune dw article

--- src/dw/dw_article.py
+++ src/dw/dw_article.py
@@ -28,25 +28,22 @@ def nonDisplayElements(browser, elements):
 def cleanPage(driver):
     logger.info('cleaning content...')
     browser = driver.getBrowser()
-    divs = browser.find_elements_by_tag_name('div')
-    for div in divs:
-        prop = div.get_attribute('itemprop')
-        if prop == 'articleBody':
-            # remove 'div', 'section'
-            elements = div.find_elements_by_tag_name('div')
-            nonDisplayElements(browser, elements)
-            elements = div.find_elements_by_tag_name('section')
-            nonDisplayElements(browser, elements)
+    # make header position fixed
+    header = browser.find_element_by_tag_name('header')
+    browser.execute_script("arguments[0].style.position = 'absolute';", header)
 
-    footer = browser.find_element_by_tag_name('footer')
-    sections = footer.find_elements_by_tag_name('section')
-    for s in sections:
-        prop = s.get_attribute('itemprop')
-        if prop != 'copyrightHolder':
-            nonDisplayElements(browser, [s])
-
-    follow = browser.find_element_by_css_selector('div.followus')
-    nonDisplayElements(browser, [follow])
-
-    cookie = browser.find_element_by_css_selector('div.cookie__wrap')
-    nonDisplayElements(browser, [cookie])
+    # <div id="DW_M_Articles_Leaderboard-label" class="advertisement advertisement--leaderboard advertisement--detail" style="display: block;">
+    # <div id="DW_M_Articles_Rectangle-1-label" class="advertisement advertisement--rectangle advertisement--detail" style="display: block;"
+    selectors = [
+        'div.advertisement__advertisement', # 广告
+        'div.followus',   # 关注我们
+        'section.offset_border',    #
+        'section.relatedsubjects',  # 主题
+        # div class="feedbackteaser feedbackteaser--linkelement"
+        'div.feedbackteaser.feedbackteaser--linkelement',   # 意见反馈
+        'section.rutscheteaser__wrap',  # 相关内容
+        'ul.footer__sectionlist',
+        'div.footer__meta',
+        'div.cookie.cookie--visible',
+    ]
+    driver.noneDisplayByCSSSelectors(selectors)
