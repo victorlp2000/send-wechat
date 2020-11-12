23c491c841ae99f2f3e3f7c3c68ec6315002f76c
Victor Liu <victorlp2000@hotmail.com>
2020-11-12 13:24:36 -0800

    using css selector to find url, better than xpath

--- src/rfi/rfi_top_story.py
+++ src/rfi/rfi_top_story.py
@@ -12,6 +12,7 @@ def findArticleUrl(driver):
     logger.info('looking for article')
     browser = driver.getBrowser()
 
-    xpath = '/html/body/div[2]/div[3]/section[1]/div/div[1]/div[1]/div/a'
-    a = browser.find_element_by_xpath(xpath)
+    selector = 'div.m-item-list-article.m-item-list-article--main-article'
+    div = browser.find_element_by_css_selector(selector)
+    a = div.find_element_by_tag_name('a')
     return a.get_attribute('href')
