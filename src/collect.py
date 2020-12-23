9858c397abf531040b351fdcfd185ccef284e57b
Victor Liu <victorlp2000@hotmail.com>
2020-12-22 22:52:56 -0800

    better format generated html

--- src/collect.py
+++ src/collect.py
@@ -14,16 +14,19 @@ from util.copy_to_contacts import copyToContacts
 def collectNews(history, timeFrom):
     logger.info('processing %s', history['fn'])
     meta = json_file.readFile(history['fn'])
+    if meta == None:
+        return None
     news = []
     for m in meta:
         title = m['title']
         dt = datetime.strptime(m['time'], '%Y-%m-%d %H:%M')
-        if dt > timeFrom:
+        if dt >= timeFrom:
             item = {}
             item['title'] = m['title']
             if 'abstract' in m:
                 item['abstract'] = m['abstract']
             news.append(item)
+    logger.info('news in <%s>: %d', history['fn'], len(news))
     if len(news) == 0:
         return None
     return { 'head': history['head'],
@@ -42,47 +45,48 @@ def main(config):
 
     now = datetime.utcnow().replace(tzinfo=pytz.utc)
     s = now.astimezone(pytz.timezone('US/Pacific')).strftime('[%Y-%m-%d %H:%M %Z]')
-    title = '新闻简报'
-    f.write('<!DOCTYPE html><html>')
-    f.write('<head><title>' + title + '</title>\n<meta charset="utf-8"/></head><body style="margin:1em">\n')
-    f.write('<style>')
-    f.write('div.head {font-weight:normal; font-size:large; color:#0000bb;}\n')
-    f.write('span.weburl {font-style:italic;}\n')
-    f.write('div.title {font-family:sans-serif; font-weight:normal;}\n')
-    f.write('div.abstract {font-family:serif; font-size:small; color:#666666;}\n')
-    f.write('ul {list-style-type:disc; padding-left:2em;}')
-    f.write('li {margin-bottom:2em;}')
-    f.write('div.dd1 {text-align:center; padding-top:20px; font-weight:bold; font-size:x-large}')
-    f.write('div.dd2 {text-align:center; padding-bottom:20px; font-size:small; color:#999999;}')
-    f.write('</style>\n')
-
-    f.write('<div class="dd1">' + title + '</div>\n')
-    f.write('<div class="dd2">' + s + '</div>\n')
+    title = '今日新闻'
+    f.write('<!DOCTYPE html>\n<html>\n')
+    f.write('<head>\n <title>' + title + '</title>\n <meta charset="utf-8"/>\n')
+    f.write(' <style>\n')
+    f.write('  div.head {font-weight:normal; font-size:large; color:#0000bb;}\n')
+    f.write('  span.weburl {font-style:italic;}\n')
+    f.write('  div.title {font-family:sans-serif; font-weight:normal;}\n')
+    f.write('  div.abstract {font-family:serif; font-size:small; color:#666666;}\n')
+    f.write('  ul {list-style-type:disc; padding-left:2em;}\n')
+    f.write('  li {margin-bottom:2em;}\n')
+    f.write('  div.dd1 {text-align:center; padding-top:20px; font-weight:bold; font-size:x-large}\n')
+    f.write('  div.dd2 {text-align:center; padding-bottom:20px; font-size:small; color:#999999;}\n')
+    f.write(' </style>\n')
+    f.write('</head>\n<body style="margin:1em">\n')
+    f.write(' <div class="dd1">' + title + '</div>\n')
+    f.write(' <div class="dd2">' + s + '</div>\n')
     # get start time from past 24 hours
     timeFrom = datetime.now() - timedelta(hours=24, minutes=0)
 
-    data = False
+    news = None
     for history in config['history']:
         news = collectNews(history, timeFrom)
         if news != None:
-            data = True
-            f.write('<div class="head">' + news['head'])
+            f.write(' <div class="head">' + news['head'])
             f.write('<span class="weburl">(' + news['url'] + ')</span></div>\n')
-            f.write('<ul>\n')
+            f.write(' <ul>\n')
             for t in news['items']:
-                f.write('<li><div class="title">' + t['title'] + '</div>')
+                f.write('  <li><div class="title">' + t['title'] + '</div>')
                 if 'abstract' in t:
                     f.write('<div class="abstract">' + t['abstract'] + '</div>')
                 f.write('</li>\n')
-            f.write('</ul>')
-    f.write('</body>')
+            f.write(' </ul>\n')
+    f.write('</body>\n</html>')
     f.close()
+    logger.info('file %s generated', tmpF)
 
     # copy generated file to contacts
-    if data:
+    if os.path.getsize(tmpF):
+        logger.info('save to contacts %s', config['contacts'])
         copyToContacts(tmpF, fn, config['contacts'])
 
-    return data
+    return
 
 if __name__ == '__main__':
     fn = os.path.basename(__file__)
